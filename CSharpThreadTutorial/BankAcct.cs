56928d1a44155b215c8b681fe442d9c3d97a6a51
kdtchw <kdtchw@gmail.com>
2021-04-01 21:29:17 +0800

    first commit

---
+++ CSharpThreadTutorial/BankAcct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpThreadTutorial
+{
+    class BankAcct
+    {
+        private Object acctLock = new object();
+        double Balance { get; set; }
+
+        public BankAcct(double bal)
+        {
+            this.Balance = bal;
+        }
+
+        public void Withdraw(double amt)
+        {
+            if ((Balance - amt) < 0)
+            {
+                Console.WriteLine($"Sorry ${Balance} in Account");
+
+            }
+            lock (acctLock)
+            {
+                if (Balance >= amt)
+                {
+                    Console.WriteLine("Removed {0} and {1} left in account", amt, (Balance - amt));
+                    Balance -= amt;
+                }
+            }
+        }
+
+        public void IssueWithThread()
+        {
+            Withdraw(1);
+        }
+    }
+}
---
+++ CSharpThreadTutorial/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CSharpThreadTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Threads counting from 1 to 10
+            Thread t = new Thread(() => PrintNumber(1));
+
+            t.Start();
+
+            PrintNumber(0);
+
+            t.Join();
+
+            Console.WriteLine();
+
+            // Threads for withdrawing from BankAcct class
+            BankAcct acct = new BankAcct(10);
+            Thread[] threads = new Thread[15];
+            Thread.CurrentThread.Name = "main";
+            for (int i = 0; i < 15; i++)
+            {
+                // ThreadStart (and ParameterizedThreadStart) delegate
+                /*
+                    Thread tmpT = new Thread(new ThreadStart(acct.IssueWithThread));
+                    Thread tmpT = new Thread(new ThreadStart(() =>
+                    {
+                        acct.Withdraw(1);
+                    }));
+                */
+                Thread tmpT = new Thread(() => { acct.Withdraw(1); });
+                tmpT.Name = i.ToString();
+                threads[i] = tmpT;
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Thread {0} isAlive: {1}", threads[i].Name, threads[i].IsAlive);
+                threads[i].Start();
+                Console.WriteLine("Thread {0} isAlive: {1}", threads[i].Name, threads[i].IsAlive);
+            }
+
+            Console.WriteLine("Current Priority: {0}", Thread.CurrentThread.Priority);
+
+            Console.ReadLine();
+        }
+
+        static void PrintNumber(int num)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // Thread.Sleep(100);
+                Console.Write(num);
+            }
+        }
+    }
+}
